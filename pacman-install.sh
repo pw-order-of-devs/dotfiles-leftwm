f59d490ae5ed01b5ee4a5867588289021f647bc1
Paweł Walus <123729832+pw-order-of-devs@users.noreply.github.com>
2023-06-19 14:09:12 +0200

    Update pacman-install.sh

--- pacman-install.sh
+++ pacman-install.sh
@@ -1,3 +1,5 @@
 pacman -S openssh git vim neovim wget curl ly feh picom polybar conky bluez bluez-utils connman \
 noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nerd-fonts-git ttf-jetbrains-mono \
-dunst libnotify zip unzip alacritty rofi fish
+dunst libnotify zip unzip alacritty rofi fish rustup
+# initialize rustup config
+rustup default stable
