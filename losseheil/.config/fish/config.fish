b156dbf13a4a06eae8096fdaba9c0ed734781d04
Pawel Walus <pawel.walus@protonmail.com>
2023-06-19 10:15:47 +0000

    fish+alacritty+rofi

---
+++ losseheil/.config/fish/config.fish
@@ -0,0 +1,22 @@
+set TERM "tmux-256color"
+
+alias pau='paru -Syu'
+alias pauu='paru -Syyuu'
+
+# neovim
+alias vim nvim
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+set -gx GTK_THEME Arc-Dark-solid
+set -gx CHROME_EXECUTABLE /usr/bin/brave
+
+fish_add_path $HOME/.npm-packages
+fish_add_path $NPM_PACKAGES/bin
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/bin
+
+set -U fish_user_paths ~/.npmbin/bin $fish_user_paths
+# starship
+starship init fish | source
+
