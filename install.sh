926d663a5c57b3e475b6a15e3900efcc3db08ae3
Paweł Walus <123729832+pw-order-of-devs@users.noreply.github.com>
2023-06-20 10:04:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,25 @@
+# set locale
+cat > /etc/localtime << EOF
+hwclock --systohc --utc
+EOF
+
+cat > /etc/locale.gen << EOF
+en_US.UTF-8 UTF-8
+pl_PL.UTF-8 UTF-8
+EOF
+
+locale-gen
+
+cat > /etc/locale.conf << EOF
+LANG=en_US.UTF-8
+EOF
+
+cat > /etc/vconsole.conf << EOF
+KEYMAP=pl
+FONT=Lat2-Terminus16.psfu.gz
+FONT_MAP=8859-2
+EOF
+
 # install base packages
 pacman -S openssh git vim neovim wget curl ly feh picom polybar conky bluez bluez-utils connman dunst libnotify zip unzip alacritty rofi fish rustup jetbrains-toolbox pavucontrol inetutils polkit
 # install fonts
@@ -5,17 +27,23 @@ pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nerd-fonts
 # initialize rustup config
 rustup default stable
 
+# install paru
 cd tmp
 git clone https://aur.archlinux.org/paru
 cd paru
 makepkg -si
 
+# fish configuration
 chsh -s $(which fish)
 
 paru -S fisher-git brave-bin leftwm betterlockscreen
 fisher install reitzig/sdkman-for-fish@v1.4.0
 
+# starship
 curl -sS https://starship.rs/install.sh | sh
+
+# sdkman
 curl -s "https://get.sdkman.io" | sh
 
+# generate ssh key
 ssh-keygen -t ed25519 -C "pawel.walus@protonmail.com"
