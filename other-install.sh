9d9dd570b1691ecb5c1a5b4d75687022cde91be4
Paweł Walus <123729832+pw-order-of-devs@users.noreply.github.com>
2023-06-20 09:18:38 +0200

    Delete other-install.sh

--- other-install.sh
+++
@@ -1,9 +0,0 @@
-chsh -s $(which fish)
-
-paru -S fisher-git brave-bin leftwm betterlockscreen
-fisher install reitzig/sdkman-for-fish@v1.4.0
-
-curl -sS https://starship.rs/install.sh | sh
-curl -s "https://get.sdkman.io" | sh
-
-ssh-keygen -t ed25519 -C "pawel.walus@protonmail.com"
