f36ef18676f435c2e3c436a1bbda20c717c99b71
Paweł Walus <123729832+pw-order-of-devs@users.noreply.github.com>
2023-06-20 11:12:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,8 +21,9 @@ EOF
 locale-gen
 timedatectl set-timezone Europe/Warsaw
 
-# install base packages
-pacman -S openssh git vim neovim wget curl ly feh picom polybar conky bluez bluez-utils connman dunst libnotify zip unzip alacritty rofi fish rustup jetbrains-toolbox pavucontrol inetutils polkit arc-gtk-theme
+# install essential packages
+pacman -S openssh git vim neovim alacritty wget curl ly feh picom polybar conky bluez bluez-utils connman dunst libnotify zip unzip fish pavucontrol inetutils polkit rofi rustup
+pacman -S jetbrains-toolbox arc-gtk-theme
 # install fonts
 pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nerd-fonts-git ttf-jetbrains-mono
 # initialize rustup config
