b42d26af88cb750bf3637db12f5884bd6979dcaa
Paweł Walus <123729832+pw-order-of-devs@users.noreply.github.com>
2023-06-20 09:18:22 +0200

    Update and rename pacman-install.sh to install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,18 @@ pacman -S openssh git vim neovim wget curl ly feh picom polybar conky bluez blue
 pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nerd-fonts-git ttf-jetbrains-mono
 # initialize rustup config
 rustup default stable
+
+cd tmp
+git clone https://aur.archlinux.org/paru
+cd paru
+makepkg -si
+
+chsh -s $(which fish)
+
+paru -S fisher-git brave-bin leftwm betterlockscreen
+fisher install reitzig/sdkman-for-fish@v1.4.0
+
+curl -sS https://starship.rs/install.sh | sh
+curl -s "https://get.sdkman.io" | sh
+
+ssh-keygen -t ed25519 -C "pawel.walus@protonmail.com"
