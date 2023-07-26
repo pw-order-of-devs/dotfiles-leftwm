2a82be1a5176281c749dd67ebbbfb0df52e2f622
Paweł Walus <123729832+pw-order-of-devs@users.noreply.github.com>
2023-07-26 10:52:59 +0200

    Update and rename install.sh to finalize.sh

--- finalize.sh
+++ finalize.sh
@@ -1,30 +1,14 @@
-# install essential packages
-sudo pacman -Syyuu
-sudo pacman -S openssh git vim neovim alacritty wget curl ly feh picom polybar conky bluez bluez-utils connman dunst libnotify zip unzip fish pavucontrol inetutils polkit network-manager-applet arc-gtk-theme rofi rustup
-# initialize rustup config
-rustup default stable
-
-# install paru
-cd /tmp
-git clone https://aur.archlinux.org/paru
-cd paru
-makepkg -si
-
-cd /tmp
-rm -rf paru
-
-# install fonts
-paru -S jetbrains-toolbox
+# install tools 
+paru -S fisher-git brave-bin leftwm betterlockscreen jetbrains-toolbox
 # install fonts
 paru -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nerd-fonts-git ttf-jetbrains-mono
 # install icon theme
 paru -S papirus-icon-theme
-
-paru -S fisher-git brave-bin leftwm betterlockscreen
-fisher install reitzig/sdkman-for-fish@v1.4.0
-
 # install cursor
 paru -S xcursor-arch-cursor-complete
+
+# fisher
+fisher install reitzig/sdkman-for-fish@v1.4.0
 
 # starship
 curl -sS https://starship.rs/install.sh | sh
